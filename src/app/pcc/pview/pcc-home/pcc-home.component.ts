fc5ec186cc2920d17e90c728720431cb97be9d11
ponindy2530 <ponmail2630@gmail.com>
2017-06-06 15:19:38 +0700

    15.19 น.

--- src/app/pcc/pview/pcc-home/pcc-home.component.ts
+++ src/app/pcc/pview/pcc-home/pcc-home.component.ts
@@ -1,13 +1,16 @@
+import { PSharedComponent } from './../../../p-shared/p-component/p-shared.component';
 import { Component, OnInit } from '@angular/core';
 declare var jQuery: any;
+import swal from 'sweetalert2';
+
 @Component({
   selector: 'app-pcc-home',
   templateUrl: './pcc-home.component.html',
   styleUrls: ['./pcc-home.component.css']
 })
-export class PccHomeComponent implements OnInit {
+export class PccHomeComponent extends PSharedComponent implements OnInit {
 
-  constructor() { }
+
 
   ngOnInit() {
   }
@@ -22,12 +25,34 @@ export class PccHomeComponent implements OnInit {
 
   getlisttopccdetail: any;
   tolistpcc(ev: any) {
-    jQuery('.modal').modal({
-      dismissible: true
-    });
+    const tokenPayload = localStorage.getItem('tokenPayload');
+    if (tokenPayload) {
+      jQuery('.modal').modal({
+        dismissible: true
+      });
 
-    this.pccdetail = true;
-    this.getlisttopccdetail = ev;
+      this.pccdetail = true;
+      this.getlisttopccdetail = ev;
+    } else {
+      this.opLogin();
+    }
+  }
+
+  opLogin() {
+    let loginPage = this._pLoginService.loginPath('login');
+    swal({
+      title: 'กรุณาเข้าสู่ระบบก่อน!',
+      text: 'เพื่อที่จะลงข้อมูลได้',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'เข้าสู่ระบบ',
+      cancelButtonText: 'ยกเลิก',
+    }).then(() => {
+      window.location.href = loginPage;
+      // console.log(loginPage);
+    });
   }
 
 }
